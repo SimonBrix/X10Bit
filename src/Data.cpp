c07ffe2aeb878120f04ce95c96f9c06504b17cf9
SimonBrix <simon.brix99@gmail.com>
2021-11-19 14:34:24 +0100

    Added Data class

---
+++ src/Data.cpp
@@ -0,0 +1,21 @@
+#include "Data.hpp"
+
+Data::Data(void* dataPtr, uint8_t bitCount) 
+{
+	
+}
+
+void Data::setData(const void* dataPtr, uint8_t bitCount) 
+{
+	
+}
+
+const void* Data::getData() const
+{
+	
+}
+
+uint8_t Data::getBitCount() const
+{
+	
+}
