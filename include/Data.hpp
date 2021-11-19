72892362bc64aa8f3ca8fbf0e2f199ff0fab4919
SimonBrix <simon.brix99@gmail.com>
2021-11-19 14:34:09 +0100

    Added Data Class

---
+++ include/Data.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+
+class Data
+{
+public:
+	Data(void* dataPtr, uint8_t bitCount);
+
+	void setData(const void* dataPtr, uint8_t bitCount);
+	const void* getData() const;
+
+	uint8_t getBitCount() const;
+
+private:
+	void* dataPtr_;
+	uint8_t bitCount_;
+
+};
