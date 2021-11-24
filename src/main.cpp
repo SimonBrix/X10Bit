4766b2b7801b3a1ac57d72452085b398d605b786
SimonBrix <simon.brix99@gmail.com>
2021-11-24 08:45:41 +0100

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-
 #include "X10Proto.hpp"
-
-/*__extension__ typedef int __guard __attribute__((mode (__DI__)));
-extern "C" {
-  int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-  void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-  void __cxa_guard_abort (__guard *) {};
-}*/
 
 extern "C" {
   void __cxa_pure_virtual(void) {};
@@ -23,8 +15,7 @@ int main()
 	PORTB = 0x00;
 
 	X10Proto x10;
-	x10.write(0b111001010);
-	//x10.test();
+	//x10.write(0b111001010);
 	while(1)
 	{}
 }
