4bbe5b0920e5590a4f71e461a480be9f91abf050
SimonBrix <simon.brix99@gmail.com>
2021-12-08 19:07:24 +0100

    Changed Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "X10Proto.hpp"
-
 extern "C" {
   void __cxa_pure_virtual(void) {};
 }
 
 int main()
 {
-	sei();
-
-	DDRB = 0xff;
-	PORTB = 0x00;
-
-	X10Proto x10;
-	//x10.write(0b111001010);
 	while(1)
 	{}
 }
