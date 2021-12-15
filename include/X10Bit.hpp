523fa45a269ad96cbcce5eefd462b49d1324684c
SimonBrix <simon.brix99@gmail.com>
2021-12-15 19:43:46 +0100

    Added XML Comments to X10Bit and constructor

--- include/X10Bit.hpp
+++ include/X10Bit.hpp
@@ -2,10 +2,11 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/// <summary> Utilizes timers and interrupts to send X10 data </summary>
 class X10Bit
 {
 public:
-	
+	/// <summary> Constructs a new X10Bit object </summary>
 	X10Bit();
 	~X10Bit();
 	void write(uint64_t , uint8_t);
